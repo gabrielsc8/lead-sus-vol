b1f984c614b51afd3a4c26a3e929ae36e70925e3
Gabriel Costa <wgabrielsc@icloud.com>
2025-08-01 17:16:10 -0300

    route fix

--- src/app/api/leads/[id]/checkin/route.ts
+++ src/app/api/leads/[id]/checkin/route.ts
@@ -1,26 +1,21 @@
-// src/app/api/leads/[id]/checkin/route.ts
-import { getServerSession } from "next-auth";
-import { authOptions } from '@/app/lib2/auth';
-import { redirect } from "next/navigation";
 import { NextResponse } from "next/server";
 import { prisma } from "@/app/lib/prisma";
 
-// Função para lidar com a requisição de check-in
 export async function PATCH(
   request: Request,
-  { params }: { params: { id: string } }
+  // A tipagem correta para o segundo argumento, que contém os parâmetros da URL.
+  context: { params: { id: string } } 
 ) {
   try {
-    const id = parseInt(params.id, 10);
+    const id = parseInt(context.params.id, 10);
     if (isNaN(id)) {
       return NextResponse.json({ message: "ID inválido." }, { status: 400 });
     }
 
-    // Atualiza o lead no banco de dados, definindo a data de check-in
     const updatedLead = await prisma.lead.update({
       where: { id },
       data: {
-        dataCheckin: new Date(), // Define a data e hora atuais
+        dataCheckin: new Date(),
       },
     });
 
